bb9049a7d4a033986b52a7942bdbcb7bce0b8608
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-09-25 18:10:39 +0900

    #1.8 Pretty URLS part One

--- components/PostLink.js
+++ components/PostLink.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default props => (
-  <Link href={`/post?title=${props.title}`}>
+  <Link href={`/post?title=${props.title}`} as={`/post/${props.title}`}>
     <a>{props.title}</a>
   </Link>
 );
