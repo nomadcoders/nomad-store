7df27b76bc89d98b58a0ce561739786600da423a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-10-07 16:41:51 +0900

    #4.6 Service Workers part Two

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,18 @@
-setInterval(() => console.log("Hi!"), 5000);
+self.addEventListener("install", event => {
+  const offlinePage = new Request("/");
+  event.waitUntil(
+    fetch(offlinePage).then(response => {
+      return caches.open("nomad-store").then(cache => {
+        return cache.put(offlinePage, response);
+      });
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(error => {
+      return caches.open("nomad-store").then(cache => cache.match("/"));
+    })
+  );
+});
